d4b77660a5474bde66979657a6c12401aca4c8bc
Renukadevi Ananthan <83960274+Renukadeviananthan14@users.noreply.github.com>
2024-01-22 22:34:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import React from 'react';
 import Header from './components/Header';
 import Content from './components/Content';
 import AboutMe from './components/AboutMe';
-import Projects from './components/Projects';
 import Contact from './components/Contact';
-import Resume from './components/Resume';
 import Footer from './components/Footer';
 
 const App = () => {
@@ -14,10 +12,10 @@ const App = () => {
       <Header />
       <Content /><hr></hr>
       <AboutMe /><hr></hr>
-      <Projects /><hr></hr>
+     
       <Contact /><hr></hr>
 
-      <Resume/>
+     
       <Footer/>
     </div>
   );
